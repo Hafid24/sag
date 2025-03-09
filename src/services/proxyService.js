9681cbdc17887d70c2491118684b55c01723a56f
hafid24 <menasria.hafidh.1>
2025-03-09 05:54:33 +0100

    handle seacrh path in proxy

--- src/services/proxyService.js
+++ src/services/proxyService.js
@@ -5,7 +5,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const apiProxy = createProxyMiddleware({
   target: TARGET_API,
   changeOrigin: true,
-  pathRewrite: { "^/": "/search.json" },
+  pathRewrite: (path, req) => {
+    console.log(path, "before");
+    let searchPath = path.replace("/?", "");
+    searchPath = "/search.json?" + searchPath;
+    searchPath += "&fields=key,title,author_name,editions,first_sentence";
+    return searchPath;
+  },
   logger: console,
 });
 
